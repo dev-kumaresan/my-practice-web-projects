2133d1b7e0ed5cd23ede835fd1fddde5abdc1b2a
Kumaresan L <100152824+dev-kumaresan@users.noreply.github.com>
2022-04-29 23:19:38 +0530

    Create index.js

---
+++ STATISTICAL-METHOS-WEB/index.js
@@ -0,0 +1,18 @@
+function Mean()
+{
+switch(document.getElementById('drop-down1').value)
+{
+    case "Mn_Individual" : 
+                window.location = "#"; 
+                break;
+    case "Mn_Discrete"   : 
+                window.location = "#"; 
+                break;      
+    case "Mn_Continuous" : 
+                window.location = "#"; 
+                break;
+    default : 
+                window.location = "#";
+                break;
+}
+} 
