cf1b3dcd95273503cb5ca7ab1660ac5307ac2e21
Kumaresan L <100152824+dev-kumaresan@users.noreply.github.com>
2022-04-30 04:45:54 +0530

    Create waitlistapp.sql

---
+++ waitlist-application/db/waitlistapp.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE waitlistapp;
+CREATE TABLE user_signup(
+    email varchar(20),
+    password varchar(15),
+    name varchar(25),
+    uniquecode varchar(5),
+    referralcode varchar(10),
+    position int(10)
+);
